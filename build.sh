17c83926f57d076bbf7f16d33b44f48944632690
Sotaro KARASAWA <sotaro.k@gmail.com>
2009-12-11 03:51:05 +0900

    build script evaluates its version automatically

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+VERSION=`cat install.rdf | grep em:version | sed -r 's/\s+?<\/?em:version>//g'`
+echo "Build Extension for version: "${VERSION}
+
 rm *.xpi
-zip -r restartbutton-$1.`date '+%Y%m%d'`.xpi * -x *.xpi *.sh *svn* *git*
+zip -r restartbutton-${VERSION}.`date '+%Y%m%d'`.xpi * -x *.xpi *.sh *svn* *git*
