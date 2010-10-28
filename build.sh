c2a9a8607a90dd495189a5150dbaf85b39d54334
Sotaro KARASAWA <sotaro.k@gmail.com>
2010-10-29 02:03:23 +0900

    update build script

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ VERSION=`cat install.rdf | grep em:version | sed -r 's/\s+?<\/?em:version>//g'`
 echo "Build Extension for version: "${VERSION}
 
 rm *.xpi
-zip -r restartbutton-${VERSION}.`date '+%Y%m%d'`.xpi * -x *.xpi *.sh *svn* *git*
+zip -r restartbutton-${VERSION}.`date '+%Y%m%d'`.xpi * -x *.xpi *.sh *svn* *git* *.DS_Store* */._*
